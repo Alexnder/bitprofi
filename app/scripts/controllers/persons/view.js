ce76c025b6de306c3439fc57ce313bf224ad26b7
Alx <a.sergienko@hadrout.com>
2016-02-14 22:56:48 +0300

    Add view for error result

--- app/scripts/controllers/persons/view.js
+++ app/scripts/controllers/persons/view.js
@@ -6,6 +6,10 @@
 angular.module('bitprofiApp')
   .controller('PersonsViewCtrl', function ($scope, $routeParams, Person) {
     Person.get({id:$routeParams.id}, function(data) {
+      if (data.error) {
+        $scope.error = data.error;
+        return;
+      }
       $scope.person = data;
     });
   });
